5e9f094c8f0ce745dbedc70fa436608ed1557d43
Maickol Rivera <m4icol.r12@gmail.com>
2024-05-30 13:56:09 -0500

    Portfolio design complete

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"CS-Dark-Background": "hsl(0, 0, 5)",
+				"CS-Dark-Secondary": "hsl(0, 0, 10)",
+				"CS-Dark-Header": "hsl(0, 0, 6)",
+				"CS-Dark-Font": "hsl(0, 0, 100)",
+				"CS-Dark-Border": "hsl(0, 0, 50)",
+
+				"CS-Light-Background": "hsl(0, 0, 98)",
+				"CS-Light-Secondary": "hsl(0, 0, 0)",
+				"CS-Light-Header": "hsl(0, 0, 97)",
+				"CS-Light-Font": "hsl(0, 0, 0)",
+				"CS-Light-Border": "hsl(0, 0, 0)"
+			}
+		},
 	},
 	plugins: [],
 }
